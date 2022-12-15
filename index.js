84948de2308d03c075e5a2fab9ba4dc283239882
MichelaKalowski <mi.ky90@hotmail.com>
2022-12-15 17:00:42 +0100

    compito

---
+++ index.js
@@ -0,0 +1,20 @@
+const valoreInput= function(e) {
+    const valIn= document.getElementById("nametask");
+    console.log(valIn);
+};
+
+
+const azionebottone= function(e) {
+    const Bottone1 =document.querySelector("button");
+
+};
+
+
+
+
+
+const Title = function () {
+    h1 = document.querySelector("h1")
+    h1.style.fontSize = "50px"
+    h1.style.color = "black"
+    }
